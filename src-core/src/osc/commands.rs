65ef7d90de6acf14ba8362b090b9df62a99f97e5
Kinse <22683778+sofoxe1@users.noreply.github.com>
2025-11-18 01:40:33 +0100

    vrchat_osc: osc interation with vrchat works

--- src-core/src/osc/commands.rs
+++ src-core/src/osc/commands.rs
@@ -1,7 +1,7 @@
-use std::{os, sync::Arc};
+use std::{net::SocketAddr, os, str::FromStr, sync::Arc, time::Duration};
 
-use log::{debug, info, trace};
-use rosc::OscType;
+use log::debug;
+use rosc::{OscMessage, OscPacket, OscType};
 use tokio::sync::Mutex;
 use vrchat_osc::{
     models::{OscNode, OscRootNode},
@@ -9,62 +9,36 @@ use vrchat_osc::{
 };
 
 use crate::{
-    osc::{
-        self,
-        models::{OSCMessage, OSCMethod, OSCValue, SupportedOscType},
-    },
+    osc::models::{OSCMessage, OSCMethod, OSCValue, SupportedOscType},
     utils,
 };
+static VRCHAT_OSC_ADDR: std::sync::Mutex<Option<SocketAddr>> = std::sync::Mutex::new(None);
+static VRCHAT_OSCQUERY_ADDR: std::sync::Mutex<Option<SocketAddr>> = std::sync::Mutex::new(None);
 //VRChatOSC internally uses RwLock
 static OSC_SERVER: Mutex<Option<Arc<VRChatOSC>>> = Mutex::const_new(None);
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
 pub async fn get_vrchat_osc_address() -> Option<String> {
-    log::debug!("get_vrchat_osc_address");
-    OSC_SERVER
+    VRCHAT_OSC_ADDR
         .lock()
-        .await
-        .as_ref()?
-        .list_mdns_services()
-        .await
-        .iter()
-        .find(|service| {
-            let name = service
-                .0
-                .to_string()
-                .starts_with("VRChat-Client")
-                .to_string();
-            name.starts_with("VRChat-Client") && name.contains("udp")
-        })
-        .map(|x| x.1.to_string())
+        .unwrap()
+        .map(|a| a.to_string())
+        .clone()
 }
 
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
 pub async fn get_vrchat_oscquery_address() -> Option<String> {
-    log::debug!("get_vrchat_oscquery_address");
-    OSC_SERVER
+    VRCHAT_OSCQUERY_ADDR
         .lock()
-        .await
-        .as_ref()?
-        .list_mdns_services()
-        .await
-        .iter()
-        .find(|service| {
-            let name = service
-                .0
-                .to_string()
-                .starts_with("VRChat-Client")
-                .to_string();
-            name.starts_with("VRChat-Client") && name.contains("tcp")
-        })
-        .map(|x| x.1.to_string())
+        .unwrap()
+        .map(|a| a.to_string())
+        .clone()
 }
 
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
 pub async fn stop_osc_server() {
-    log::debug!("stop_osc_server");
     if let Some(server) = OSC_SERVER.lock().await.take() {
         server.shutdown().await.ok();
     }
@@ -74,18 +48,15 @@ pub async fn stop_osc_server() {
 #[oyasumivr_macros::command_profiling]
 
 pub async fn start_osc_server() -> Option<(String, String)> {
-    log::debug!("start_osc_server");
-    //todo: fix
     Some((
-        String::from("i'm so confused"),
-        String::from("how this works"),
+        get_vrchat_osc_address().await?,
+        get_vrchat_oscquery_address().await?,
     ))
 }
 
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
 pub async fn add_osc_method(method: OSCMethod) {
-    log::debug!("add_osc_method");
     #[cfg(debug_assertions)]
     if !WHITELIST.lock().await.contains(&method.address) {
         panic!("{:?}", method);
@@ -106,22 +77,43 @@ pub async fn osc_send_command(
     types: Vec<SupportedOscType>,
     values: Vec<String>,
 ) -> Result<bool, String> {
-    unimplemented!("{:?}\n{:?}\n{:?}\n:{:?}", addr, osc_addr, types, values);
+    OSC_SERVER
+        .lock()
+        .await
+        .as_mut()
+        .unwrap()
+        .send_to_addr(
+            OscPacket::Message(OscMessage {
+                addr: osc_addr,
+                args: values
+                    .into_iter()
+                    .enumerate()
+                    .map(|(idx, value)| match types[idx] {
+                        SupportedOscType::Int => OscType::Int(value.parse().unwrap()),
+                        SupportedOscType::Float => OscType::Float(value.parse().unwrap()),
+                        SupportedOscType::Boolean => OscType::Bool(value.parse().unwrap()),
+                        SupportedOscType::String => OscType::String(value),
+                    })
+                    .collect(),
+            }),
+            SocketAddr::from_str(&addr).unwrap(),
+        )
+        .await
+        .map_err(|err| err.to_string())?;
+    Ok(true)
 }
 
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
 pub async fn osc_valid_addr(addr: String) -> bool {
-    unimplemented!("{:?}", addr);
+    SocketAddr::from_str(&addr).is_ok()
 }
-// async fn osc_send(addr: String, osc_addr: String, data: Vec<OscType>) -> Result<bool, String> {
-//     unimplemented!("{:?}\n{:?}\n{:?}",addr,osc_addr,data);
-// }
 #[cfg(debug_assertions)]
 static WHITELIST: Mutex<Vec<String>> = Mutex::const_new(Vec::new());
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
 pub async fn set_osc_receive_address_whitelist(whitelist: Vec<String>) {
+    let mut server_guard = OSC_SERVER.lock().await;
     #[cfg(debug_assertions)]
     {
         *WHITELIST.lock().await = whitelist.clone();
@@ -135,31 +127,58 @@ pub async fn set_osc_receive_address_whitelist(whitelist: Vec<String>) {
             ..Default::default()
         })
     }
-    vrchat_osc
-        .register("OyasumiVR", root_node, |msg| {
-            // info!("Received OSC message:{:?}", msg); //trace is broken for some reason
-            match msg {
-                rosc::OscPacket::Message(osc_message) => {
-                    if osc_message.args.len() > 1 {
-                        unimplemented!("{:?}", osc_message);
-                    }
-                    let msg= osc_message.args.into_iter().map(|arg| match arg {
-                            OscType::Int(v) => OSCValue{ kind: "int".into(), value: Some(v.to_string()) },
-                            OscType::Float(v) => OSCValue{ kind: "float".into(), value: Some(v.to_string()) },
-                            OscType::String(v) => OSCValue{ kind: "string".into(), value: Some(v.to_string()) },
-                            OscType::Bool(v) => OSCValue{ kind: "bool".into(), value: Some(v.to_string()) },
-                            _=>unimplemented!("{:?}",arg),
-                        }).collect::<Vec<_>>();
-                    tokio::task::spawn(utils::send_event(
-                        "OSC_MESSAGE",
-                       OSCMessage{ address: osc_message.addr, values:msg},
-                    ));
-                }
-                rosc::OscPacket::Bundle(osc_bundle) => unimplemented!(),
+    vrchat_osc.on_connect(|type_| {
+        match type_ {
+            vrchat_osc::ServiceType::Osc(_, socket_addr) => {
+                VRCHAT_OSC_ADDR.lock().unwrap().replace(socket_addr)
             }
+            vrchat_osc::ServiceType::OscQuery(_, socket_addr) => {
+                VRCHAT_OSCQUERY_ADDR.lock().unwrap().replace(socket_addr)
+            }
+        };
+    }).await;
+    vrchat_osc
+        .register("OyasumiVR", root_node, |msg| match msg {
+            rosc::OscPacket::Message(osc_message) => {
+                if osc_message.args.len() > 1 {
+                    unimplemented!("{:?}", osc_message);
+                }
+                let msg = osc_message
+                    .args
+                    .into_iter()
+                    .map(|arg| match arg {
+                        OscType::Int(v) => OSCValue {
+                            kind: "int".into(),
+                            value: Some(v.to_string()),
+                        },
+                        OscType::Float(v) => OSCValue {
+                            kind: "float".into(),
+                            value: Some(v.to_string()),
+                        },
+                        OscType::String(v) => OSCValue {
+                            kind: "string".into(),
+                            value: Some(v.to_string()),
+                        },
+                        OscType::Bool(v) => OSCValue {
+                            kind: "bool".into(),
+                            value: Some(v.to_string()),
+                        },
+                        _ => unimplemented!("{:?}", arg),
+                    })
+                    .collect::<Vec<_>>();
+                tokio::task::spawn(utils::send_event(
+                    "OSC_MESSAGE",
+                    OSCMessage {
+                        address: osc_message.addr,
+                        values: msg,
+                    },
+                ));
+            }
+            rosc::OscPacket::Bundle(_osc_bundle) => unimplemented!(),
         })
         .await
         .ok()
         .unwrap();
-    OSC_SERVER.lock().await.replace(vrchat_osc);
+    server_guard.replace(vrchat_osc);
+    tokio::time::sleep(Duration::from_secs(1)).await
 }
