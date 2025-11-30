6f1825a422795671689bde32ca5097938dd33b5a
Kinse <22683778+sofoxe1@users.noreply.github.com>
2025-11-30 21:17:17 +0100

    fix build

--- scripts/pre-build-linux.js
+++ scripts/pre-build-linux.js
@@ -6,11 +6,11 @@ async function main() {
   const SourceFile = 'src-core/resources/fonts';
   const TargetDirectory = 'src-core/target/release/resources/fonts';
   await mkdirp(SourceFile);
-  await copy(SourceFile, TargetDirectory, { overwrite: false });
+  await copy(SourceFile, TargetDirectory, { overwrite: true });
   const SourceFile2 = 'src-core/resources/sounds';
   const TargetDirectory2 = 'src-core/target/release/resources/sounds';
   await mkdirp(SourceFile2);
-  await copy(SourceFile2, TargetDirectory2, { overwrite: false });
+  await copy(SourceFile2, TargetDirectory2, { overwrite: true });
 }
 
 main().catch((e) => {
