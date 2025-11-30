bd4b23a747e15596d8bec5e0b8a7276a28b33184
Kinse <22683778+sofoxe1@users.noreply.github.com>
2025-11-30 05:51:21 +0100

    linux: fix build

---
+++ scripts/pre-build-linux.js
@@ -0,0 +1,18 @@
+import { mkdirp } from 'mkdirp';
+import copy from 'recursive-copy';
+import { rimraf } from 'rimraf';
+
+async function main() {
+  const SourceFile = 'src-core/resources/fonts';
+  const TargetDirectory = 'src-core/target/release/resources/fonts';
+  await mkdirp(SourceFile);
+  await copy(SourceFile, TargetDirectory, { overwrite: false });
+  const SourceFile2 = 'src-core/resources/sounds';
+  const TargetDirectory2 = 'src-core/target/release/resources/sounds';
+  await mkdirp(SourceFile2);
+  await copy(SourceFile2, TargetDirectory2, { overwrite: false });
+}
+
+main().catch((e) => {
+  throw e;
+});
