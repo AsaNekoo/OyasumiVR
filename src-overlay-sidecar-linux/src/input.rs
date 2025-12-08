241c31b903b0f7c66456a80a2f1da45d2798af63
Kinse <22683778+sofoxe1@users.noreply.github.com>
2025-12-08 07:28:14 +0100

    bump xr library

--- src-overlay-sidecar-linux/src/input.rs
+++ src-overlay-sidecar-linux/src/input.rs
@@ -1,13 +1,19 @@
-use xr_overlay::runner::builders::{ControllersCreateInfo, ProfileControllersCreateInfo};
+use std::fs;
 
-use crate::vr::BINDING_FILE_PATH;
+use xr_overlay::runner::builders::ControllersCreateInfo;
+
+use crate::{
+    ARGS,
+    vr::{BINDING_FILE_PATH, DEFAULT_BINDINGS_CONFIG},
+};
 
 pub fn get_controller_create_info() -> ControllersCreateInfo {
-    let bindings = ControllersCreateInfo {
-        profiles: [ProfileControllersCreateInfo::default_oculus_touch()].into(),
-    };
+    let bindings = ControllersCreateInfo::default();
     let bindings_ = bindings.clone();
-    match bindings.parse_config(BINDING_FILE_PATH.to_path_buf()) {
+    match bindings.parse_config(match ARGS.get().as_ref().unwrap().core_pid == 0 {
+        true => DEFAULT_BINDINGS_CONFIG.to_string(),
+        false => fs::read_to_string(BINDING_FILE_PATH.as_path()).unwrap(),
+    }) {
         Ok(v) => v,
         Err(err) => {
             log::error!("failed to parse binding config with:{:?}", err);
