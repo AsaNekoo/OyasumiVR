fd8ad03b255f4678360d8a0357bd6636f99586c0
Kinse <22683778+sofoxe1@users.noreply.github.com>
2025-12-24 02:15:51 +0100

    fix incorrect gpu profile log message refering to msi afterburner

--- src-ui/app/components/event-log/event-log-entry/entry-parsers/msi-afterburner-profile-set.ts
+++ src-ui/app/components/event-log/event-log-entry/entry-parsers/msi-afterburner-profile-set.ts
@@ -1,5 +1,6 @@
 import { EventLogEntryParser } from '../event-log-entry-parser';
 import { EventLogMsiAfterburnerProfileSet, EventLogType } from '../../../../models/event-log-entry';
+import { is_windows } from 'src-ui/app/app.module';
 
 export class EventLogMsiAfterburnerProfileSetEntryParser extends EventLogEntryParser<EventLogMsiAfterburnerProfileSet> {
   entryType(): EventLogType {
@@ -17,6 +18,10 @@ export class EventLogMsiAfterburnerProfileSetEntryParser extends EventLogEntryPa
   }
 
   override headerInfoSubTitle(entry: EventLogMsiAfterburnerProfileSet): string {
+    if (is_windows){
     return 'comp.event-log-entry.type.msiAfterburnerProfileSet.reason.' + entry.reason;
+    }else{
+      return 'comp.event-log-entry.type.LactProfileSet.reason.' + entry.reason;
+    }
   }
 }
