AsaNekoo/OyasumiVR

commit f234fa93e7a912cc69834b975597a736446704f7

increase sleep detector report frequncy to not mess with calibaration

src-core/src/vr/sleep_detector.rs
@@ -51,7 +51,7 @@ impl SleepDetector {
 
         // Send a state report if it's been over a second since the last one
         if now > self.next_state_report {
-            if now.saturating_sub(self.last_log) > 60000 {
+            if now.saturating_sub(self.last_log) > 10000 {
                 self.start_time = now;
             }
             const LAST_10_SECS_EVENT_COUNT:usize=(Duration::from_secs(10).as_secs_f32()/SLEEP_DETECTOR_PERIOD.as_secs_f32()) as usize;

commit 196ff01bcc8c7fe211c3e093d816e65744b150d0

fix sleep detection broken by f234fa93e7a912cc69834b975597a736446704f7

src-core/src/vr/commands.rs
@@ -5,7 +5,14 @@ use super::openvr::{
 };
 #[cfg(windows)]
 use crate::globals::STEAM_APP_KEY;
-use crate::{vr::{SLEEP_DETECTION_ENABLED, model::{BindingOriginData, OVRDevice, OVRFrameLimits, SleepState}}, warn_unimplemented};
+use crate::{
+    vr::{
+        model::{BindingOriginData, OVRDevice, OVRFrameLimits, SleepState},
+        openxr::SLEEP_DETECTOR,
+        SLEEP_DETECTION_ENABLED,
+    },
+    warn_unimplemented,
+};
 #[cfg(windows)]
 use enumset::EnumSet;
 #[cfg(windows)]
@@ -16,10 +23,12 @@ use ovr::input::{InputString, InputValueHandle};
 use ovr_overlay as ovr;
 #[cfg(windows)]
 use substring::Substring;
-pub static mut SLEEP_STATE:SleepState=SleepState::Awake;
+pub static mut SLEEP_STATE: SleepState = SleepState::Awake;
 #[tauri::command]
-pub async fn set_sleep_state(state:SleepState){
-    unsafe {SLEEP_STATE=state;}
+pub async fn set_sleep_state(state: SleepState) {
+    unsafe {
+        SLEEP_STATE = state;
+    }
 }
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
@@ -44,17 +53,23 @@ pub async fn vr_get_app_framelimit(app_id: u32) -> Result<Option<OVRFrameLimits>
 }
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
-pub async fn vr_sleep_mode_check(value:bool){
+pub async fn vr_sleep_mode_check(value: bool) {
     #[cfg(unix)]
-    {match value {
-        true => super::openxr::start_head_shake_detection().await,
-        false => super::openxr::stop_head_shake_detection().await,
-    }}
+    {
+        match value {
+            true => super::openxr::start_head_shake_detection().await,
+            false => super::openxr::stop_head_shake_detection().await,
+        }
+    }
 }
 #[tauri::command]
-pub async fn vr_sleep_detection_enabled(value:bool)
-{
-    unsafe{SLEEP_DETECTION_ENABLED=value;}
+pub async fn vr_sleep_detection_enabled(value: bool) {
+    unsafe {
+        if !SLEEP_DETECTION_ENABLED && value {
+            SLEEP_DETECTOR.lock().await.reset_start_time().await;
+        }
+        SLEEP_DETECTION_ENABLED = value;
+    }
 }
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
@@ -81,10 +96,18 @@ pub async fn vr_get_devices() -> Vec<OVRDevice> {
 pub async fn vr_status() -> String {
     #[cfg(windows)]
     {
-       super::openvr::OVR_STATUS.lock().await.to_string().to_uppercase()
+        super::openvr::OVR_STATUS
+            .lock()
+            .await
+            .to_string()
+            .to_uppercase()
     }
     #[cfg(unix)]
-    super::openxr::OXR_STATE.lock().await.to_string().to_uppercase()
+    super::openxr::OXR_STATE
+        .lock()
+        .await
+        .to_string()
+        .to_uppercase()
 }
 
 #[tauri::command]
@@ -139,15 +162,12 @@ pub async fn openvr_get_fade_distance() -> Result<f32, String> {
 #[tauri::command]
 #[oyasumivr_macros::command_profiling]
 #[cfg_attr(unix, allow(unused_variables))]
-pub async fn vr_set_analog_color_temp(
-    temperature: Option<u32>,
-) -> Result<(f64, f64, f64), String> {
+pub async fn vr_set_analog_color_temp(temperature: Option<u32>) -> Result<(f64, f64, f64), String> {
     #[cfg(windows)]
     return super::colortemp_analog::set_color_temp(temperature).await;
     #[cfg(unix)]
     warn_unimplemented!();
     Err("not implemented".into())
-
 }
 
 #[tauri::command]
@@ -157,8 +177,11 @@ pub async fn vr_set_image_brightness(
     perceived_brightness_adjustment_gamma: Option<f64>,
 ) {
     #[cfg(windows)]
-    super::openvr::brightness_overlay::set_brightness(brightness, perceived_brightness_adjustment_gamma)
-        .await;
+    super::openvr::brightness_overlay::set_brightness(
+        brightness,
+        perceived_brightness_adjustment_gamma,
+    )
+    .await;
     #[cfg(unix)]
     super::openxr::set_brightness(brightness, perceived_brightness_adjustment_gamma).await;
 }
@@ -200,12 +223,11 @@ pub async fn openvr_is_dashboard_visible() -> bool {
         };
         return manager.is_dashboard_visible();
     }
-        #[cfg(unix)]
-        {
-            //there is no one dashboard on openxr
-            false
-        }
-    
+    #[cfg(unix)]
+    {
+        //there is no one dashboard on openxr
+        false
+    }
 }
 
 #[tauri::command]
@@ -260,7 +282,8 @@ pub async fn openvr_reregister_manifest() -> Result<(), String> {
             }
         }
     }
-    #[cfg(unix)]{
+    #[cfg(unix)]
+    {
         //not used
         Ok(())
     }
@@ -417,7 +440,8 @@ pub async fn vr_get_binding_origins(
 
         Some(datas)
     }
-    #[cfg(unix)]{
+    #[cfg(unix)]
+    {
         //fixme: look at it and see wha it does
         None
     }

src-core/src/vr/openxr/mod.rs
@@ -319,7 +319,7 @@ pub async fn set_brightness(brightness: f64, perceived_brightness_adjustment_gam
 fn adjust_for_perceived_brightness(linear_percent: f64, gamma: f64) -> f64 {
     linear_percent.powf(1.0 / gamma)
 }
-static SLEEP_DETECTOR: LazyLock<Mutex<SleepDetector>> =
+pub static SLEEP_DETECTOR: LazyLock<Mutex<SleepDetector>> =
     LazyLock::new(|| Mutex::new(SleepDetector::new()));
-static GESTURE_DETECTOR: LazyLock<Mutex<GestureDetector>> =
+pub static GESTURE_DETECTOR: LazyLock<Mutex<GestureDetector>> =
     LazyLock::new(|| Mutex::new(GestureDetector::new()));

src-core/src/vr/sleep_detector.rs
@@ -1,4 +1,7 @@
-use std::{collections::VecDeque, time::Duration};
+use std::{
+    collections::VecDeque,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
 use glam::Vec3;
 
@@ -7,28 +10,30 @@ use crate::{
     vr::model::SleepDetectorStateReport,
 };
 
-pub const SLEEP_DETECTOR_PERIOD:Duration=Duration::from_millis(300);
+pub const SLEEP_DETECTOR_PERIOD: Duration = Duration::from_millis(300);
 const MAX_EVENT_AGE_MS: Duration = Duration::from_mins(15); // 15 minutes
-
-
 
 pub struct SleepDetector {
     disatnces: VecDeque<f32>,
-    last_pos:Vec3,
+    last_pos: Vec3,
     distance_in_last_15_minutes: f32,
     distance_in_last_10_seconds: f32,
     start_time: u64,
     last_log: u64,
     next_state_report: u64,
 }
-const EVENT_COUNT:usize=(MAX_EVENT_AGE_MS.as_secs_f32()/SLEEP_DETECTOR_PERIOD.as_secs_f32()) as usize;
+const EVENT_COUNT: usize =
+    (MAX_EVENT_AGE_MS.as_secs_f32() / SLEEP_DETECTOR_PERIOD.as_secs_f32()) as usize;
 impl SleepDetector {
     pub fn new() -> Self {
         Self {
-            disatnces: VecDeque::with_capacity(EVENT_COUNT+1),
+            disatnces: VecDeque::with_capacity(EVENT_COUNT + 1),
             distance_in_last_10_seconds: 0.0,
             distance_in_last_15_minutes: 0.0,
-            start_time: 0,
+            start_time: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_millis() as u64,
             last_log: 0,
             next_state_report: 0,
             last_pos: Vec3::default(),
@@ -38,35 +43,44 @@ impl SleepDetector {
     pub async fn log_pose(&mut self, position: Option<Vec3>) {
         let now = get_time();
         // Add the event
-        if let Some(position)=position{
-        self.disatnces.push_back(position.distance(self.last_pos));
-        self.last_pos=position;
-        }else {
+        if let Some(position) = position {
+            self.disatnces.push_back(position.distance(self.last_pos));
+            self.last_pos = position;
+        } else {
             self.disatnces.push_back(0.); //treat failed posef query as no movement
         }
-        if self.disatnces.len()>EVENT_COUNT{
+        if self.disatnces.len() > EVENT_COUNT {
             self.disatnces.pop_front();
         }
 
-
         // Send a state report if it's been over a second since the last one
         if now > self.next_state_report {
-            if now.saturating_sub(self.last_log) > 10000 {
-                self.start_time = now;
-            }
-            const LAST_10_SECS_EVENT_COUNT:usize=(Duration::from_secs(10).as_secs_f32()/SLEEP_DETECTOR_PERIOD.as_secs_f32()) as usize;
+            const LAST_10_SECS_EVENT_COUNT: usize = (Duration::from_secs(10).as_secs_f32()
+                / SLEEP_DETECTOR_PERIOD.as_secs_f32())
+                as usize;
             // const LAST_15_MINS_EVENT_COUNT:usize=(Duration::from_mins(15).as_secs_f32()/SLEEP_DETECTOR_PERIOD.as_secs_f32()) as usize;
-            self.distance_in_last_10_seconds = self.disatnces.iter().rev().take(LAST_10_SECS_EVENT_COUNT).sum();
+            self.distance_in_last_10_seconds = self
+                .disatnces
+                .iter()
+                .rev()
+                .take(LAST_10_SECS_EVENT_COUNT)
+                .sum();
 
             // self.distance_in_last_15_minutes =self.disatnces.iter().rev().take(LAST_15_MINS_EVENT_COUNT).sum();
-            self.distance_in_last_15_minutes =self.disatnces.iter().sum();
+            self.distance_in_last_15_minutes = self.disatnces.iter().sum();
 
             self.last_log = now;
-            self.next_state_report = now + 30000;
+            self.next_state_report = now + Duration::from_secs(10).as_millis() as u64;
             self.send_state_report().await;
         }
     }
-
+    //called when enabling sleep detection after it was disabled
+    pub async fn reset_start_time(&mut self) {
+        self.start_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u64;
+    }
 
     async fn send_state_report(&self) {
         send_event(
@@ -81,6 +95,4 @@ impl SleepDetector {
         .await;
         // self.send_influxdb_report();
     }
-
-
 }

src-overlay-sidecar-linux/.gitignore
@@ -1,2 +1,3 @@
 target/
+*.log
 