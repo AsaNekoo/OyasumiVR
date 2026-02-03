b62517989c7fa7865ff62f57c3671db5939a0544
sofoxe1 <22683778+sofoxe1@users.noreply.github.com>
2026-02-03 12:42:53 +0100

    strip downloaded cef

--- src-core/src/utils/linux_download_cef.sh
+++ src-core/src/utils/linux_download_cef.sh
@@ -4,4 +4,5 @@ wget -qO- "https://cef-builds.spotifycdn.com/cef_binary_142.0.14%2Bgceaf578%2Bch
 mv cef_binary_142.0.14+gceaf578+chromium-142.0.7444.163_linux64_minimal/ cef
 mv cef/Resources/* cef/
 mv cef/Release/* cef/
+strip cef/*.so
 mv cef/ resources/sidecars/cef
